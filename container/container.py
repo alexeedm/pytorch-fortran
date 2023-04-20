45979887f64e7454772fcaca66a4c29f4a0c9eec
Dmitry Alexeev <dalexeev@nvidia.com>
2023-04-20 15:26:23 -0700

    Updated container.py with the new compiler versions

--- container/container.py
+++ container/container.py
@@ -57,17 +57,25 @@ def build_nvhpc(stage, args):
 
     stage += nvhpc
 
+    # 23.1 pulls 23.3 for some reason, we'll remove it
+    if (nvhpc._nvhpc__version == '23.1'):
+        stage += hp.shell(commands = [
+            r'''sed -ie 's/, nvhpc-2023 (>= 23.1)//g' /var/lib/dpkg/status''',
+            r'apt autoremove -y'
+        ])
+
     # Remove NV HPC bundled CUDA and point the compilers to the Torch CUDA
     stage += hp.shell(commands = [
         r'''cuda_path=$(find /opt/nvidia/hpc_sdk/Linux_x86_64/*/cuda -maxdepth 1 -name '??.?' -type d)''',
         r'rm -r $cuda_path',
         r'ln -s /usr/local/cuda $cuda_path'
     ])
-    logging.info(f'Cleaning up HPC SDK package to reduce the image size, recommended to build with --squash')
+    logging.info(f'Cleaning up HPC SDK package to reduce the image size, '+
+                 f'recommended to build with docker-squash (https://github.com/goldmann/docker-squash)')
 
     # Workaround for the libgomp OpenACC issue
     stage += hp.environment(variables = {
-        'LD_PRELOAD' : r'$LD_PRELOAD:/opt/nvidia/hpc_sdk/Linux_x86_64/22.7/compilers/lib/libaccnotify.so'
+        'LD_PRELOAD' : f'$LD_PRELOAD:/opt/nvidia/hpc_sdk/Linux_x86_64/{nvhpc._nvhpc__version}/compilers/lib/libaccnotify.so'
     })
 
     add_common_packages(stage)
@@ -82,8 +90,8 @@ def build_gnu(stage, args):
     if (not pytorch_tag):
         try:
             from natsort import natsorted
-            response = json.loads(requests.get(r'https://registry.hub.docker.com/v1/repositories/pytorch/pytorch/tags').text)
-            versions = [cont['name'] for cont in response if ('devel' in cont['name'] and 'nightly' not in cont['name'])]
+            response = json.loads(requests.get(r'https://registry.hub.docker.com/v2/repositories/pytorch/pytorch/tags').text)
+            versions = [cont['name'] for cont in response['results'] if ('devel' in cont['name'] and 'nightly' not in cont['name'])]
             pytorch_tag = natsorted(versions)[-1]
         except:
             pass
@@ -103,15 +111,27 @@ def build_intel(stage, args):
     import requests
     import re
 
-    stage += hp.baseimage(image = f'intel/intel-optimized-pytorch', _distro='ubuntu20', _as='build')
+    if (args.format == 'singularity'):
+        logging.critical(f'Intel container needs Docker format. You may convert the Dockerfile to Singularity recipe later')
+
+    if (not args.pytorch_tag):
+        args.pytorch_tag = 'latest'
+
+    stage += hp.baseimage(image = f'intel/intel-optimized-pytorch:{args.pytorch_tag}', _distro='ubuntu20', _as='build')
     add_common_packages(stage)
+    # Intel pytorch container don't seem to use conda anymore
+    stage += hbb.apt_get(ospackages=['python3-dev'])
+    stage += hp.shell(commands = [
+        'python -m pip install pybind11'
+    ])
 
     oneapi_docker = requests.get(r'https://raw.githubusercontent.com/intel/oneapi-containers/master/images/docker/hpckit/Dockerfile.ubuntu-20.04').text
     oneapi_patched = re.sub('FROM ubuntu.*as build', '', oneapi_docker)
     oneapi_patched = re.sub('COPY third-party-programs\.txt.*', '', oneapi_patched)
     oneapi_patched = re.sub('intel-oneapi-python', '', oneapi_patched)
     oneapi_patched = re.sub('ENV CONDA_.*', '', oneapi_patched)
-    oneapi_patched = re.sub('FROM ubuntu:.*', 'FROM intel/intel-optimized-pytorch', oneapi_patched)
+    oneapi_patched = re.sub('FROM ubuntu:.*', 'FROM build', oneapi_patched)
+    oneapi_patched = re.sub('COPY --from=build.*', '', oneapi_patched)
     oneapi_patched = re.sub('ENV PYTHONPATH=.*', '', oneapi_patched)
     oneapi_patched = re.sub('/opt/intel/oneapi/intelpython/latest/bin:', '', oneapi_patched)
     oneapi_patched = re.sub('/opt/intel/oneapi/intelpython/latest/condabin:', '', oneapi_patched)
@@ -122,23 +142,23 @@ def build_intel(stage, args):
 
 ################################################
 
-parser = argparse.ArgumentParser(description='Genererate recipe for the bindings build environment')
+parser = argparse.ArgumentParser(description='Genererate recipe for the bindings build environment',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--format', type=str, default='docker',
                     choices=['docker', 'singularity'],
-                    help='Container specification format, default is docker')
+                    help='Container specification format')
 parser.add_argument('--recipe-file', type=argparse.FileType('w'), default='./Dockerfile',
-                    help='Output recipe file, default is ./Dockerfile')
+                    help='Output recipe file')
 parser.add_argument('--build-bindings', action='store_true',
                     help='Build the binding in the container, by default only the development environment will be built')
 
 subparsers = parser.add_subparsers(title='Compilers', dest='compiler')
 subparsers.required = True
 
-p_gnu = subparsers.add_parser('gnu', help='GNU compilers: gcc and gfortran')
-p_gnu.set_defaults(function=build_gnu)
-
-p_gnu = subparsers.add_parser('intel', help='Intel compilers: icc and ifort')
-p_gnu.set_defaults(function=build_intel)
+p_intel = subparsers.add_parser('intel', help='Intel compilers: icc and ifort')
+p_intel.set_defaults(function=build_intel)
+p_intel.add_argument('--pytorch-tag', type=str, default=None, \
+                    help='Pytorch container tag, see https://hub.docker.com/r/intel/intel-optimized-pytorch, default is the latest available')
 
 p_gnu = subparsers.add_parser('gnu', help='GNU compilers: gcc and gfortran')
 p_gnu.add_argument('--pytorch-tag', type=str, default=None, \
